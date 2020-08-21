da4b84683a709127d28e297ac02712f96d201619
Ibrahim <adeniyiib360@gmail.com>
2020-08-21 14:34:25 +0100

    soooooooon............

--- components/add-new-post/QuillEditor.js
+++ components/add-new-post/QuillEditor.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import ReactQuill from "react-quill"
-import "react-quill/dist/quill.snow.css"
 
 const QuillWYSIWYG = ({ id, setRawHtml, value }) => {
   const modules = {
