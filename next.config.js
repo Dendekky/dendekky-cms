b2a7efd8ef0b2759f35593e49c97cf0de916a187
Ibrahim <adeniyiib360@gmail.com>
2020-09-14 16:07:48 +0100

    pwa attempt 3

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const withPWA = require("next-pwa")
 
 module.exports = withPWA({
   pwa: {
-    dest: "public",
+    // dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    scope: "/app",
+    sw: "service-worker.js",
   },
 })
 
