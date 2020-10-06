a5399f347517f0cd5539a3223453a427e470e5f4
Ibrahim <adeniyiib360@gmail.com>
2020-10-06 16:10:08 +0100

    lint changes :bomb:

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,15 +1,8 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
-import React, { useState } from 'react'
-import Link from 'next/link'
-import {
-  Col,
-  Card,
-  CardBody,
-  Badge,
-  CardFooter,
-  Tooltip,
-} from 'shards-react'
-import { trimmedPostBody } from '../utils/trimmedPostBody'
+import React, { useState } from "react"
+import Link from "next/link"
+import { Col, Card, CardBody, Badge, CardFooter, Tooltip } from "shards-react"
+import { trimmedPostBody } from "../utils/trimmedPostBody"
 
 function PostCard({ post, idx, postIdList }) {
   const [postToggleOpen, setPostToggleOpen] = useState(postIdList)
@@ -23,20 +16,20 @@ function PostCard({ post, idx, postIdList }) {
   }
 
   return (
-    <Col lg='6' md='6' sm='12' className='mb-4'>
+    <Col lg="6" md="6" sm="12" className="mb-4">
       <Link href={`/post/${post.slug}`} as={`/post/${post.slug}`}>
-        <a className='text-fiord-blue'>
-          <Card small className='card-post card-post--1'>
+        <a className="text-fiord-blue" style={{ textDecoration: "none" }}>
+          <Card small className="card-post card-post--1">
             <div
-              className='card-post__image'
+              className="card-post__image"
               style={{ backgroundImage: `url(${post.postImage})` }}
             >
-              <Badge pill className='card-post__category bg-primary'>
+              <Badge pill className="card-post__category bg-primary">
                 {post.category}
               </Badge>
             </div>
             <CardBody id={`post-${post._id}`}>
-              <h4 className='card-title'>{trimmedPostBody(post.title, 50)}</h4>
+              <h4 className="card-title">{trimmedPostBody(post.title, 50)}</h4>
               <div
                 dangerouslySetInnerHTML={{
                   __html: trimmedPostBody(post.body, 120),
@@ -45,8 +38,8 @@ function PostCard({ post, idx, postIdList }) {
             </CardBody>
             <Tooltip
               style={{
-                minWidth: '320px',
-                fontSize: '20px',
+                minWidth: "320px",
+                fontSize: "20px",
               }}
               // placement="bottom"
               open={postToggleOpen[idx].tooltipOpen}
@@ -56,7 +49,7 @@ function PostCard({ post, idx, postIdList }) {
               {post.title}
             </Tooltip>
             <CardFooter>
-              <span className='text-muted'>
+              <span className="text-muted">
                 {new Date(post.updatedAt).toDateString()}
               </span>
             </CardFooter>
